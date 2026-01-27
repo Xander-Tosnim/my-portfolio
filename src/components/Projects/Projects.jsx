0f3110a26e46318914c01da69ce2b1f6e2fdc769
Xander-Tosnim <tosnimila69@gmail.com>
2026-01-27 16:24:52 +0600

    add projects in "Projects" container

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,22 @@
 import './Projects.css'
 
+const projects = [
+    {name: "BookNow", url: "https://booknow-upni.onrender.com", sample:"/book-now.png", descripton:"A React project from Codecademy for bookmarking calendar"}
+]
+
 function Projects() {
 
     return(
         <div className="projects">
             <h2>&lt;Projects /&gt;</h2>
+            <div className='project-list'>
+                {projects.map((project, index) => {
+                    <div key={index} className='project-card'>
+                        <img src={project.sample} alt={project.name} />
+                        <h3>{project.name}</h3>
+                    </div>
+                })}
+            </div>
         </div>
     );
 }
